17ef714c417a064bbac93a43b5efbf415243e769
Isidoro <itorrico.m@gmail.com>
2024-01-17 16:57:20 -0400

    variables, input, output, condicional if

---
+++ app.js
@@ -0,0 +1,23 @@
+let numeroSecreto = 6;
+let numeroUsario = prompt("Dame un numero entre 1 y 10: ");
+console.log(numeroUsario);
+
+if (numeroSecreto == numeroUsario) {
+    alert("GAME OVER");
+}
+
+let mensajeDeBienvenida = 'Bienvenido a la aplicación';
+alert('mensajeDeBienvenida');
+
+alert("¡Bienvenida y bievenido a nuestro sitio web!");
+let nombre = 'Lunar';
+let edad = 25;
+let numeroDeVentas = 5;
+let saldoDisponible = 1000;
+alert("¡Error! Completa todos los campos");
+let mensajeDeError  = "¡Error! Completa todos los campos";
+nombre = prompt('Ingrese el nombre:');
+edad = prompt('Ingrese la edad:');
+if (edad >= 18){
+    alert('¡Puedes obtener tu licencia de conducir!');
+}
